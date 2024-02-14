8b280b7086ba256be66d6a6d31085c08122bacda
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-02-14 14:37:46 +0530

    React and tailwind setup

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
