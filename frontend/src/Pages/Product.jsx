20e513dc8391afe51328809041fe8fc25656fa2a
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-02-15 16:31:19 +0530

    Product redirection

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./CSS/Product.css";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
+  return(
+    <div className="product">
+      <Breadcrum product = {product}/>
+    </div>
+  
+  );
 };
 
 export default Product;
