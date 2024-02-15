465537c69391676c2b81e2b98afb3c6be016076c
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-02-15 20:13:40 +0530

    Cart Total functionality and UI

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
+  const { addToCart } = useContext(ShopContext);
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
@@ -41,21 +43,27 @@ const ProductDisplay = (props) => {
           cotton shirt with button-down collar and tailored fit.
         </div>
         <div className="productdisplay-right-size">
-            <h1>Select Size</h1>
-            <div className="productdisplay-right-sizes">
-                <div>S</div>
-                <div>M</div>
-                <div>L</div>
-                <div>XL</div>
-                <div>XXL</div>
-            </div>
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
         </div>
-        <button>ADD TO CART</button>
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          ADD TO CART
+        </button>
         <p className="productdisplay-right-category">
-            <span>Category :</span> Women, T-Shirt, Crop Top
+          <span>Category :</span> Women, T-Shirt, Crop Top
         </p>
         <p className="productdisplay-right-category">
-            <span>Tags :</span> Modern, Latest, Crop Top
+          <span>Tags :</span> Modern, Latest, Crop Top
         </p>
       </div>
     </div>
