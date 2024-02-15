fc1c2c95ac4933f4108c99485664c5b6938b79a5
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-02-15 15:39:00 +0530

    signup ui

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>Accept Terms and conditions</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
