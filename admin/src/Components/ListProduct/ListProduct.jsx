26c3dedd0cf8517025338eea6c83987d9fdcbd0b
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-02-18 21:57:27 +0530

    Add product and list product

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ListProduct.css";
+
+const ListProduct = () => {
+  return (
+    <div className="list-product">
+      <p>Product Title</p>
+      <input type="text" name="name" placeholder="Type here" />
+    </div>
+  );
+};
+
+export default ListProduct;
